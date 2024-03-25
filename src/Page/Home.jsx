ceef4a871ac4efb26fa31c6fa8991809d0eac694
Atamazow <atamazov99@mail.ru>
2024-03-25 14:58:59 +0300

    Сделал проект. в процессе header. Ставлю видео

---
+++ src/Page/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import "../styles.css";
+function Home(props) {
+  return (
+    <div className="wrapper">
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
