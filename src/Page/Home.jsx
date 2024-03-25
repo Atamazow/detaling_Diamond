16b5043eaab16214a126ec3120a3de7e1da65585
Atamazow <atamazov99@mail.ru>
2024-03-25 15:33:53 +0300

    Сделал фоное видое

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -3,8 +3,25 @@ import Header from "../components/Header/Header";
 import "../styles.css";
 function Home(props) {
   return (
-    <div className="wrapper">
-      <Header />
+    <div>
+      <div className="intro">
+        <div className="intro-media">
+          <video
+            src="https://storage.yandexcloud.net/lupo/%D0%A4%D0%B0%D0%B9%D0%BB%D1%8B%20%D0%B4%D0%BB%D1%8F%20%D1%81%D0%B0%D0%B9%D1%82%D0%BE%D0%B2%20%D0%BA%D0%BB%D0%B8%D0%B5%D0%BD%D1%82%D0%BE%D0%B2/Re5/%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F.mp4#t=0.001"
+            muted
+            loop
+            autoPlay
+            className="video_media"
+          >
+            das
+          </video>
+        </div>
+        <div className="intro_content_wrapper">
+          <div className="intro_content">
+            <Header />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,24 +10,6 @@ function Header(props) {
         <div className={style.logoDD}>DD</div>
       </div>
       <div className={style.logoTitle}>Detailing diamond</div>
-      <div className="video"></div>
-      <video
-        poster="https://static.tildacdn.com/tild3032-3136-4539-b436-626231643930/Rectangle_16.png"
-        muted
-        loop
-        autoPlay
-        playsInline=""
-        preload="metadata"
-        pip="false"
-        className="video_media"
-        onLoadedMetadata="this.muted = true"
-      >
-        <source
-          src="https://storage.yandexcloud.net/lupo/%D0%A4%D0%B0%D0%B9%D0%BB%D1%8B%20%D0%B4%D0%BB%D1%8F%20%D1%81%D0%B0%D0%B9%D1%82%D0%BE%D0%B2%20%D0%BA%D0%BB%D0%B8%D0%B5%D0%BD%D1%82%D0%BE%D0%B2/Re5/%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F.mp4#t=0.001"
-          type="video/mp4"
-        />
-        das
-      </video>
     </div>
   );
 }
