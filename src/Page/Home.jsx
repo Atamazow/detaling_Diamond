32194e54bffb3c4d02d43061496b7c3d414eeeca
Atamazow <atamazov99@mail.ru>
2024-03-25 16:32:08 +0300

    Сделал фоное видое

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -7,7 +7,7 @@ function Home(props) {
       <div className="intro">
         <div className="intro-media">
           <video
-            src="https://storage.yandexcloud.net/lupo/%D0%A4%D0%B0%D0%B9%D0%BB%D1%8B%20%D0%B4%D0%BB%D1%8F%20%D1%81%D0%B0%D0%B9%D1%82%D0%BE%D0%B2%20%D0%BA%D0%BB%D0%B8%D0%B5%D0%BD%D1%82%D0%BE%D0%B2/Re5/%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F.mp4#t=0.001"
+            src="https://brooklandsdetailing.ru/images/site_main_brooklands_1.mp4"
             muted
             loop
             autoPlay
