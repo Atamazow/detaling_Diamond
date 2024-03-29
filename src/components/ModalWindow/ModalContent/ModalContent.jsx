aff88ae49eebacdea2147097e022d737b7ca56eb
Atamazow <atamazov99@mail.ru>
2024-03-29 17:03:17 +0300

    Изменил стили

--- src/components/ModalWindow/ModalContent/ModalContent.jsx
+++ src/components/ModalWindow/ModalContent/ModalContent.jsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./ModalContent.module.css";
 function ModalContent(props) {
+  const form = useRef();
+  const sendEmail = () => {};
   return (
     <div className={styles.wrapper_data}>
       <p className={styles.data_title}>
         Оставьте свой email и мы, <br /> отправим письмо в ближайшее время
       </p>
-      <div className={styles.name}>
-        <input className={styles.dataSize} placeholder="Имя..." type="text" />
-      </div>
-      <div className={styles.lastName}>
-        <input
-          className={styles.dataSize}
-          placeholder="Фамилия..."
-          type="text"
-        />
-      </div>
-      <div className={styles.email}>
-        <input className={styles.dataSize} placeholder="email..." type="text" />
-      </div>
-      <div className={styles.dataBtn}>Отправить заявку</div>
+      <form ref={form} action="" onSubmit={sendEmail}>
+        <div className={styles.name}>
+          <input className={styles.dataSize} placeholder="Имя..." type="text" />
+        </div>
+        <div className={styles.lastName}>
+          <input
+            className={styles.dataSize}
+            placeholder="Фамилия..."
+            type="text"
+          />
+        </div>
+        <div className={styles.email}>
+          <input
+            className={styles.dataSize}
+            placeholder="email..."
+            type="text"
+          />
+        </div>
+        <div className={styles.dataBtn}>Отправить заявку</div>
+      </form>
     </div>
   );
 }
