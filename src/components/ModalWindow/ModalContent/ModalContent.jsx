9e4a9cc2b013c06f83466ee65f066916e32140a9
Atamazow <atamazov99@mail.ru>
2024-03-29 17:26:43 +0300

    Сделал функционал для отправки письмо на почту

--- src/components/ModalWindow/ModalContent/ModalContent.jsx
+++ src/components/ModalWindow/ModalContent/ModalContent.jsx
@@ -1,32 +1,62 @@
 import React, { useRef } from "react";
 import styles from "./ModalContent.module.css";
+import emailjs from "@emailjs/browser";
+
 function ModalContent(props) {
   const form = useRef();
-  const sendEmail = () => {};
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_d2haskt", "template_qmpagsq", form.current, {
+        publicKey: "GaTDZac-xe_f3iQ2G",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        },
+      );
+    e.target.reset();
+  };
+
   return (
     <div className={styles.wrapper_data}>
       <p className={styles.data_title}>
         Оставьте свой email и мы, <br /> отправим письмо в ближайшее время
       </p>
-      <form ref={form} action="" onSubmit={sendEmail}>
+      <form ref={form} onSubmit={sendEmail}>
         <div className={styles.name}>
-          <input className={styles.dataSize} placeholder="Имя..." type="text" />
-        </div>
-        <div className={styles.lastName}>
           <input
+            name="user_name"
             className={styles.dataSize}
-            placeholder="Фамилия..."
+            placeholder="Имя..."
             type="text"
           />
         </div>
+        {/*<div className={styles.lastName}>*/}
+        {/*  <input*/}
+        {/*    className={styles.dataSize}*/}
+        {/*    placeholder="Фамилия..."*/}
+        {/*    type="text"*/}
+        {/*  />*/}
+        {/*</div>*/}
         <div className={styles.email}>
           <input
+            name="user_email"
             className={styles.dataSize}
             placeholder="email..."
             type="text"
           />
         </div>
-        <div className={styles.dataBtn}>Отправить заявку</div>
+        <textarea name="message" />
+        <input
+          type="submit"
+          value="Отправить заявку"
+          className={styles.dataBtn}
+        ></input>
       </form>
     </div>
   );
