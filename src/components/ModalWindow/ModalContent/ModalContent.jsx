704ba88d9f51570798a9cb6e3201c75d7f24e8de
Atamazow <atamazov99@mail.ru>
2024-03-29 14:32:04 +0300

    Сделал модальное окно

--- src/components/ModalWindow/ModalContent/ModalContent.jsx
+++ src/components/ModalWindow/ModalContent/ModalContent.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-
+import styles from "./ModalContent.module.css";
 function ModalContent(props) {
   return (
-    <div>
-      <div>
-        <input type="text" />
+    <div className={styles.wrapper_data}>
+      <p className={styles.data_title}>
+        Оставьте свой email и мы, <br /> отправим письмо в ближайшее время
+      </p>
+      <div className={styles.name}>
+        <input className={styles.dataSize} placeholder="Имя..." type="text" />
       </div>
-      <div>
-        <input type="text" />
+      <div className={styles.lastName}>
+        <input
+          className={styles.dataSize}
+          placeholder="Фамилия..."
+          type="text"
+        />
       </div>
+      <div className={styles.email}>
+        <input className={styles.dataSize} placeholder="email..." type="text" />
+      </div>
+      <div className={styles.dataBtn}>Отправить заявку</div>
     </div>
   );
 }
--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -3,18 +3,18 @@ import styles from "./ModalWindow.module.css";
 import CloseIcon from "@mui/icons-material/Close";
 import ModalContent from "./ModalContent/ModalContent";
 
-function ModalWindow({ closeModal, setIsModalOpen }) {
-  // useEffect(() => {
-  //   const closeModalClick = (e) => {
-  //     if (e.target.id === "modal_backdrop") closeModal();
-  //   };
-  //   window.addEventListener("click", closeModalClick);
-  //   return () => window.removeEventListener("click", closeModalClick);
-  // }, [isModalOpen]);
-  // if (!isModalOpen) return null;
-  const handleModalClick = () => setIsModalOpen(false);
+function ModalWindow({ closeModal, setIsModalOpen, isModalOpen }) {
+  useEffect(() => {
+    const closeModalClick = (e) => {
+      if (e.target.id === "modal_backdrop") closeModal();
+    };
+    window.addEventListener("click", closeModalClick);
+    return () => window.removeEventListener("click", closeModalClick);
+  }, [isModalOpen]);
+  if (!isModalOpen) return null;
+
   return (
-    <div onClick={handleModalClick} className={styles.modal_backdrop}>
+    <div className={styles.modal_backdrop}>
       <div className={styles.modal}>
         <div>
           <ModalContent />
