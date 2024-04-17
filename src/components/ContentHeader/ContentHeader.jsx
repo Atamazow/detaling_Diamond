e37d049701e057cce1a321cbe0afe94edc94bf6f
Atamazow <atamazov99@mail.ru>
2024-04-17 11:15:36 +0300

    Сделал активный компонент другие услуги, при клике открывается модальное окно

--- src/components/ContentHeader/ContentHeader.jsx
+++ src/components/ContentHeader/ContentHeader.jsx
@@ -1,49 +1,45 @@
-import React, { useState } from "react";
-import { motion } from "framer-motion";
-import Header from "../Header/Header";
-import logoPhone from "../../assets/icon/WDB+Fractal+Diamond.png";
-import ModalWindow from "../ModalWindow/ModalWindow";
+import { motion } from 'framer-motion'
+import React, { useState } from 'react'
+import logoPhone from '../../assets/icon/WDB+Fractal+Diamond.png'
+import Header from '../Header/Header'
+import ModalWindow from '../ModalWindow/ModalWindow'
 
 function ContentHeader(props) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [showText, setShowText] = useState(false);
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+	const [isModalOpen, setIsModalOpen] = useState(false)
+	const [showText, setShowText] = useState(false)
+	const openModal = () => setIsModalOpen(true)
+	const closeModal = () => setIsModalOpen(false)
 
-  return (
-    <div>
-      <div className="intro_content_wrapper">
-        {isModalOpen && (
-          <ModalWindow
-            isModalOpen={isModalOpen}
-            setIsModalOpen={setIsModalOpen}
-            closeModal={closeModal}
-          />
-        )}
-        <div className="intro_content">
-          <Header />
-        </div>
-      </div>
-      <div className="logoPhone_wrapper">
-        <div className="logoPhoneDiamond">
-          <img className="logoPhone" src={logoPhone} alt="" />
-        </div>
-      </div>
-      <motion.div
-        // translateX={{ 20 }}
-        className="header_title_text"
-      >
-        <div className="header_title">Детейлинг высокого класса</div>
-        <p className="header_text">
-          Для самых требовательных автолюбителей Исключительный уход за кузовом
-          и салоном вашего автомобиля
-        </p>
-        <div className="header_btn" onClick={openModal}>
-          Записаться
-        </div>
-      </motion.div>
-    </div>
-  );
+	return (
+		<div>
+			<div className='intro_content_wrapper'>
+				{isModalOpen && (
+					<ModalWindow isModalOpen={isModalOpen} closeModal={closeModal} />
+				)}
+				<div className='intro_content'>
+					<Header />
+				</div>
+			</div>
+			<div className='logoPhone_wrapper'>
+				<div className='logoPhoneDiamond'>
+					<img className='logoPhone' src={logoPhone} alt='' />
+				</div>
+			</div>
+			<motion.div
+				// translateX={{ 20 }}
+				className='header_title_text'
+			>
+				<div className='header_title'>Детейлинг высокого класса</div>
+				<p className='header_text'>
+					Для самых требовательных автолюбителей Исключительный уход за кузовом
+					и салоном вашего автомобиля
+				</p>
+				<div className='header_btn' onClick={openModal}>
+					Записаться
+				</div>
+			</motion.div>
+		</div>
+	)
 }
 
-export default ContentHeader;
+export default ContentHeader
--- src/components/MainContent/SoundWashing/SoundWashing.jsx
+++ src/components/MainContent/SoundWashing/SoundWashing.jsx
@@ -1,7 +1,12 @@
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt'
-import React from 'react'
+import React, { useState } from 'react'
+import ModalWindow from '../../ModalWindow/ModalWindow'
 import style from './SoundWashing.module.css'
 function SoundWashing() {
+	const [isModalOpen, setIsModalOpen] = useState(false)
+	const openModal = () => setIsModalOpen(true)
+	const closeModal = () => setIsModalOpen(false)
+
 	return (
 		<div className={style.SoundWashingWrapper}>
 			<div className={style.wrapperSound}>
@@ -16,7 +21,10 @@ function SoundWashing() {
 					<ArrowRightAltIcon className={style.iconArrowRightIcon} alt='' />
 				</div>
 			</div>
-			<div className={style.wrapperSound}>
+			{isModalOpen && (
+				<ModalWindow closeModal={closeModal} isModalOpen={isModalOpen} />
+			)}
+			<div onClick={openModal} className={style.wrapperSound}>
 				<div className={style.SoundTitle}>другие ислуги</div>
 				<div className={style.iconSoundWashing}>
 					<ArrowRightAltIcon className={style.iconArrowRightIcon} alt='' />
