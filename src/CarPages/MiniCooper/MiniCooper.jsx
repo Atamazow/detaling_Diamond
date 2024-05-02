9bb9c757b6953dfff07fdb7f95ddaa7983e8b4a0
Atamazow <atamazov99@mail.ru>
2024-05-02 15:00:04 +0300

    Сделал отдельную страницу ддя машины, наложил тень

--- src/CarPages/MiniCooper/MiniCooper.jsx
+++ src/CarPages/MiniCooper/MiniCooper.jsx
@@ -1,20 +1,20 @@
-// import React from 'react'
-// import Footer from '../../components/Footer/Footer'
-// import Header from '../../components/Header/Header'
-// import style from './MiniCooper.module.css'
+import React from 'react'
+import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
+import style from './MiniCooper.module.css'
 
-// function MiniCooper() {
-// 	return (
-// 		<>
-// 			<div className={style.carImage}>
-// 				<div className={style.shadowLayer}></div> {/* Слой тени */}
-// 				<div className={style.carContent}>
-// 					<Header />
-// 				</div>
-// 			</div>
-// 			<Footer />
-// 		</>
-// 	)
-// }
+function MiniCooper() {
+	return (
+		<>
+			<div className={style.carImage}>
+				<div className={style.shadowLayer}></div> {/* Слой тени */}
+				<div className={style.carContent}>
+					<Header />
+				</div>
+			</div>
+			<Footer />
+		</>
+	)
+}
 
-// export default MiniCooper
+export default MiniCooper
